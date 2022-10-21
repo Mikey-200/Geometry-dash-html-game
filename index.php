77f802b00421ce44edcf48b9237d81ac1d1c57ca
mike <114485426+Mikey-200@users.noreply.github.com>
2022-10-21 12:55:02 +0100

    cv

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 
     <?php
-    $x = $_POST["test"];
-    if (isset($x)) {
-    echo "welcome".$x;
-    $y = fopen("name.txt", "w");
-    fwrite($y, $x);
-    fclose($y);
-  }
+      if (isset($_POST['test'])) {
+        $email_to = "michaelchidera2005@gmail.com";
+        $email_subject = "New form submissions";
+        echo "hiiii";
+      }
+      if (!isset($_POST['test'])) {
+        echo "error with the request";
+      }
     ?>
