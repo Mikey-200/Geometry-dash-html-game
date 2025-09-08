651a11df53a1ab2b49f31a48d8271e39cf8df754
Michael Chidera <michaelchidera2005@gmail.com>
2025-09-08 23:48:23 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("geometry-rush-v1").then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/clicktone.wav",
+                "/eatcointone.wav",
+                "/backgroundmusic.mp3",
+                "/collisiontone.wav",
+                "/icon-192.png",
+                "/icon-512.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
